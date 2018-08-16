336447bd39d49c39fc73acaeab6b23abb066e6a8
tranki1 <kimhieutran100@gmail.com>
2018-08-16 12:25:07 +0300

    Add static files monitoring

--- gulpfile.js
+++ gulpfile.js
@@ -9,6 +9,8 @@ var gulp = require("gulp"),
 var coffeeSources = ["components/coffee/*.coffee"];
 var jsSources = ["components/scripts/*.js"];
 var sassSources = ["components/sass/style.scss"];
+var htmlSources = ["builds/development/*.html"];
+var jsonSources = ["builds/development/js/*.json"];
 
 gulp.task("coffee", function() {
   gulp
@@ -45,6 +47,8 @@ gulp.task("watch", function() {
   gulp.watch(coffeeSources, ["coffee"]);
   gulp.watch(jsSources, ["js"]);
   gulp.watch("components/sass/*.scss", ["compass"]);
+  gulp.watch(htmlSources, ["html"]);
+  gulp.watch(jsonSources, ["json"]);
 });
 
 gulp.task("connect", function() {
@@ -54,4 +58,20 @@ gulp.task("connect", function() {
   });
 });
 
-gulp.task("default", ["coffee", "js", "compass", "connect", "watch"]);
+gulp.task("html", function() {
+  gulp.src(htmlSources).pipe(connect.reload());
+});
+
+gulp.task("json", function() {
+  gulp.src(jsonSources).pipe(connect.reload());
+});
+
+gulp.task("default", [
+  "html",
+  "json",
+  "coffee",
+  "js",
+  "compass",
+  "connect",
+  "watch"
+]);
